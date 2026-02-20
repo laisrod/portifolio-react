360a7758328433b4cd47fd272aabcf3031ed9a38
Laís Rodrigues <devlaisrodrigues@gmail.com>
2026-02-20 18:19:17 -0300

    refactor: remove ProjectLinks section
    - Delete ProjectLinks.tsx component and ProjectLinks.css
    - Remove import and usage from App.tsx
    - Remove CSS import from main.css

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Hero from './components/sections/Hero'
 import About from './components/sections/About'
 import Skills from './components/sections/Skills'
 import Projects from './components/sections/Projects'
-import ProjectLinks from './components/sections/ProjectLinks'
 import Contact from './components/sections/Contact'
 
 function App() {
@@ -19,7 +18,6 @@ function App() {
         <About />
         <Skills />
         <Projects />
-        <ProjectLinks />
         <Contact />
       </main>
       <Footer />
